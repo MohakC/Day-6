063ac81f029ee49ee395be40b13cdf35a959b4f4
MohakC <chadha6@purdue.edu>
2017-05-23 13:30:52 -0400

    Improve how ThingList and Thing look

--- src/Thing.js
+++ src/Thing.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import './Thing.css'
 
 const Thing = ({ thing }) => {
-    return (
-        <li className="thing">{thing.name}</li>
-    )
+  return (
+    <li className="Thing">
+      <input type="checkbox" value="on" />
+      <div className="details">
+        <div className="name">
+          {thing.name}
+        </div>
+        <span className="actions">
+          <button className="remove">
+            <i className="fa fa-trash-o"></i>
+          </button>
+        </span>
+      </div>
+    </li>
+  )
 }
 
 export default Thing
--- src/ThingList.js
+++ src/ThingList.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import Thing from './Thing'
 import './ThingList.css'
+import Thing from './Thing'
 
 const ThingList = (props) => {
-    return (
-        <ul className="ThingList">
-          { Object
-            .keys(props.things)
-            .map(thingID => <Thing thing={props.things[thingID]} key={thingID}/>)
-          }
-        </ul>
-    )
+  return (
+    <ul className="ThingList">
+      {
+        Object
+          .keys(props.things)
+          .map(thingId => <Thing thing={props.things[thingId]} key={thingId} />)
+      }
+    </ul>
+  )
 }
 
 export default ThingList
